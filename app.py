3a6564033239e443477f266b1fb8e018c9c88f77
Trish <trishajjohnson@gmail.com>
2021-05-20 23:40:13 -0500

    working on saving answers to RESPONSES list and resetting at start of survey

--- app.py
+++ app.py
@@ -1,8 +1,81 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkey'
-
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+RESPONSES = []
+
+
+@app.route('/')
+def home_page():
+
+    """Shows homepage with start survey button"""
+    
+    RESPONSES = []
+    print("This is the response after returning back from end of survey", RESPONSES)
+    return render_template('start_survey.html', survey=survey)
+
+
+@app.route('/start-survey', methods=["POST"])
+def start_survey():
+
+    """Clears RESPONSES of previous answers, sets to an empty list.  Then redirects to first question of survey."""
+    print('Is this printing?')
+    
+    # RESPONSES = []
+    
+    # print('RESPONSES', RESPONSES)
+
+    return redirect('/questions/0')
+
+
+@app.route('/answer', methods=["POST"])
+def handle_answer():
+
+    """Retrieves answer from form, appends answer to RESPONSES list and redirects to next question"""
+
+    print('RESPONSES before answer appended', RESPONSES)
+    answer = request.form["answer"]
+    RESPONSES.append(answer)
+    print('RESPONSES after answer appended', RESPONSES)
+
+    if len(RESPONSES) < len(survey.questions):
+        num = len(RESPONSES)
+        return redirect(f'/questions/{num}')
+
+    else:
+        return redirect('/survey-complete')
+
+
+
+@app.route('/questions/<int:qnum>')
+def display_question(qnum):
+
+    """Takes you to the first question of the survey after the start button is clicked"""
+    
+    question = survey.questions[qnum]
+    num = qnum
+
+    if RESPONSES is None:
+        return redirect('/')
+    
+    if len(RESPONSES) == len(survey.questions):
+        return redirect('/survey-complete')
+
+    if len(RESPONSES) != num:
+        return redirect(f'/questions/{ len(RESPONSES) }')
+
+    return render_template('question.html', num=num, question=question)
+
+
+@app.route('/survey-complete')
+def survey_complete_msg():
+
+    """"Displays survey complete message once survey has been completed"""
+    print(RESPONSES)
+    return render_template('survey-complete.html')
